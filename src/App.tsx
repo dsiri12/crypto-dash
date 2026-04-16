a0f40a2f7c3f9d0180818d7b26a8b667cf4fd159
Damien Siri <dsiri12@gmail.com>
2026-04-15 20:26:11 -0400

    limit selector

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 // import { coinsMarketsData } from "./apiData/coinsMarketsData";
 import type { CoinType } from "./types/CoinType";
 import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
 
 const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_CG_DEMO_API_KEY;
@@ -10,11 +11,14 @@ const App = () => {
   const [coins, setCoins] = useState<CoinType[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<null | string>(null);
+  const [limit, setLimit] = useState(10);
+
+  const setLimit2 = (limit2: number) => setLimit(limit2);
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const url = `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+        const url = `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`;
 
         const res = await fetch(url, {
           headers: {
@@ -38,13 +42,15 @@ const App = () => {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
 
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit2} />
 
       {!loading && !error && (
         <main className="grid">
---
+++ src/components/LimitSelector.tsx
@@ -0,0 +1,28 @@
+type Props = {
+  limit: number;
+  onLimitChange: (limit2: number) => void;
+};
+
+// const LimitSelector = (props: Props) => {
+//   const {limit, onLimitChange} = props
+const LimitSelector = ({ limit, onLimitChange }: Props) => {
+  return (
+    <div className="controls">
+      <label htmlFor="limit">Show: </label>
+
+      <select
+        value={limit}
+        id="limit"
+        onChange={(e) => onLimitChange(Number(e.target.value))}
+      >
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+    </div>
+  );
+};
+
+export default LimitSelector;
